0e3a736712411c6f1ea6aff071ba7295f236f1d6
Vinu Cyril <vinucyril@gmail.com>
2023-05-06 15:39:18 +0530

    font-selection

--- src/Nav.js
+++ src/Nav.js
@@ -1,15 +1,53 @@
+import { useState, useEffect } from "react";
 import logo from "./assets/images/logo.svg";
 import arrowdown from "./assets/images/icon-arrow-down.svg";
 import moon from "./assets/images/icon-moon.svg";
+
 const Nav = () => {
+  const [font, setFont] = useState("sans-serif");
+  const handleFonts = (param) => {
+    setFont(param);
+  };
+
+  useEffect(() => {
+    document.body.classList.forEach((className) => {
+      document.body.classList.remove(className);
+    });
+    document.body.classList.add(font);
+  }, [font]);
+
+  const handleFontSelection = () => {
+    document.querySelector(".fonts").classList.toggle("show");
+  };
+
   return (
     <nav>
       <img src={logo}></img>
       <div className="nav-end">
-        <div className="font-selection">
+        <div onClick={handleFontSelection} className="font-selection">
           <div className="font-selected">
-            <p>Sans Serif</p>
+            <p>{font}</p>
             <img src={arrowdown}></img>
+            <div className="fonts">
+              <p
+                onClick={() => handleFonts("sans-serif")}
+                style={{ fontFamily: "sans-serif" }}
+              >
+                Sans Serif
+              </p>
+              <p
+                onClick={() => handleFonts("Serif")}
+                style={{ fontFamily: "serif" }}
+              >
+                Serif
+              </p>
+              <p
+                onClick={() => handleFonts("mono")}
+                style={{ fontFamily: "Mono" }}
+              >
+                Mono
+              </p>
+            </div>
           </div>
         </div>
         <label className="toggle">
